15e125f03d7ca57ff4debe1a9b9e418065c4ea34
Quico Moya <me@qmoya.com>
2021-12-30 14:48:17 +0100

    Format code

--- Package.swift
+++ Package.swift
@@ -2,31 +2,34 @@
 import PackageDescription
 
 let package = Package(
-    name: "swift-wall-calendar",
-    products: [
-        .library(
-            name: "WallCalendar",
-            targets: ["WallCalendar"]),
-    ],
-    dependencies: [
-		.package(
-		  url: "https://github.com/apple/swift-collections.git",
-		  .upToNextMajor(from: "1.0.0") // or `.upToNextMinor
-		),
-		
-		.package(url: "https://github.com/pointfreeco/swift-snapshot-testing.git", from: "1.9.0")
-    ],
-    targets: [
-        .target(
-            name: "WallCalendar",
-            dependencies: [
-				.product(name: "OrderedCollections", package: "swift-collections")
-			]),
-        .testTarget(
-            name: "WallCalendarTests",
-            dependencies: [
-				"WallCalendar",
-				.product(name: "SnapshotTesting", package: "swift-snapshot-testing"),
-			]),
-    ]
+  name: "swift-wall-calendar",
+  products: [
+    .library(
+      name: "WallCalendar",
+      targets: ["WallCalendar"])
+  ],
+  dependencies: [
+    .package(
+      url: "https://github.com/apple/swift-collections.git",
+      .upToNextMajor(from: "1.0.0")  // or `.upToNextMinor
+    ),
+
+    .package(url: "https://github.com/pointfreeco/swift-snapshot-testing.git", from: "1.9.0"),
+  ],
+  targets: [
+    .target(
+      name: "WallCalendar",
+      dependencies: [
+        .product(name: "OrderedCollections", package: "swift-collections")
+      ]),
+    .testTarget(
+      name: "WallCalendarTests",
+      dependencies: [
+        "WallCalendar",
+        .product(name: "SnapshotTesting", package: "swift-snapshot-testing"),
+      ],
+      exclude: [
+        "__Snapshots__"
+      ]),
+  ]
 )
